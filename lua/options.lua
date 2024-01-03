7ca815fe5cf0935ee172d17f9322e52a6381a3ba
Guanran Wang <guanran928@outlook.com>
2024-01-04 05:06:27 +0800

    use relative line number

--- lua/options.lua
+++ lua/options.lua
@@ -8,6 +8,7 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 vim.wo.number = true
+vim.wo.relativenumber = true
 vim.wo.signcolumn = "yes"
 vim.o.undofile = true
 vim.o.clipboard = "unnamedplus"
