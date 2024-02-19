46844b9f267e853622d30d8d3d258d60de75433d
Guanran Wang <guanran928@outlook.com>
2024-02-20 00:58:17 +0800

    repo: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Guanran928's nvim config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system: let
+        inherit (inputs.nixpkgs) lib;
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
+      in {
+        packages.default = let
+          runtimeDeps = with pkgs; [gcc];
+        in (
+          pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped
+          (pkgs.neovimUtils.makeNeovimConfig
+            {
+              customRC = ''
+                set runtimepath^=${./.}
+                source ${./.}/init.lua
+              '';
+            }
+            // {
+              wrapperArgs = ["--prefix" "PATH" ":" "${lib.makeBinPath runtimeDeps}"];
+            })
+        );
+
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.stylua
+          ];
+        };
+      }
+    );
+}
