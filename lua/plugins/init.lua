39bb7b9e08995d9dbcbbf90ab315c61b6fd1565c
Guanran Wang <guanran928@outlook.com>
2024-02-21 10:52:04 +0800

    telescope: simplify

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -73,7 +73,16 @@ require("lazy").setup({
   },
 
   -- telescope.lua
-  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+    keys = {
+      { "<Space>f", "<cmd>Telescope find_files<CR>", desc = "Find files" },
+      { "<Space><Space>", "<cmd>Telescope oldfiles<CR>", desc = "Recent files" },
+      { "<Space>sg", "<cmd>Telescope live_grep<CR>", desc = "Live grep" },
+    },
+  },
 
   -- treesitter.lua
   {
@@ -165,6 +174,3 @@ require("lazy").setup({
 require("plugins.lspconfig") -- utilizes lsp
 require("plugins.completions") -- adds completions
 require("plugins.treesitter") -- better highlights
-
--- QoL
-require("plugins.telescope") -- file search
--- lua/plugins/telescope.lua
+++
@@ -1,8 +0,0 @@
-require("telescope").setup()
-local builtin = require("telescope.builtin")
-
-vim.keymap.set("n", "<Space>f", builtin.find_files, {})
-vim.keymap.set("n", "<Space><Space>", builtin.oldfiles, {})
-
-vim.keymap.set("n", "<Space>sg", builtin.live_grep, {})
-vim.keymap.set("n", "<Space>sh", builtin.help_tags, {})
