1b2784874b5b1c6cbbccaea9a05cbddb500a4aa4
Guanran Wang <guanran928@outlook.com>
2024-02-23 16:38:41 +0800

    nvim-treesitter: format

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,24 +1,16 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  init = function()
-    require("nvim-treesitter.configs").setup({
-      -- A list of parser names, or "all"
-      ensure_installed = {
-        "nix",
-        "lua",
-        "vim",
-      },
+  opts = {
+    ensure_installed = {
+      "nix",
+      "lua",
+      "vim",
+    },
 
-      -- Install parsers synchronously (only applied to `ensure_installed`)
-      sync_install = false,
-      auto_install = true,
-      highlight = {
-        enable = true,
-      },
-      indent = {
-        enable = true,
-      },
-    })
-  end,
+    sync_install = true,
+    auto_install = true,
+    highlight = { enable = true },
+    indent = { enable = true },
+  },
 }
