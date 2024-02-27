ce9dffb2865405740026bb26259e78863083acd7
Guanran Wang <guanran928@outlook.com>
2024-02-27 14:34:15 +0800

    colorscheme: use borderless telescope

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,7 +2,41 @@ return {
   "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
-  opts = {},
+  opts = {
+    -- Borderless telescope
+    -- see: https://github.com/folke/tokyonight.nvim?tab=readme-ov-file#borderless-telescope-example
+    on_highlights = function(hl, c)
+      -- local prompt = "#2d3149"
+      local prompt = c.bg_highlight
+      hl.TelescopeNormal = {
+        bg = c.bg_dark,
+        fg = c.fg_dark,
+      }
+      hl.TelescopeBorder = {
+        bg = c.bg_dark,
+        fg = c.bg_dark,
+      }
+      hl.TelescopePromptNormal = {
+        bg = prompt,
+      }
+      hl.TelescopePromptBorder = {
+        bg = prompt,
+        fg = prompt,
+      }
+      hl.TelescopePromptTitle = {
+        bg = prompt,
+        fg = prompt,
+      }
+      hl.TelescopePreviewTitle = {
+        bg = c.bg_dark,
+        fg = c.bg_dark,
+      }
+      hl.TelescopeResultsTitle = {
+        bg = c.bg_dark,
+        fg = c.bg_dark,
+      }
+    end,
+  },
   init = function()
     vim.cmd([[ colorscheme tokyonight-night ]])
   end,
