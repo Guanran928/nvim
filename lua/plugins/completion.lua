92228a6538cd013b586e279b9f5a62926b601b88
Guanran Wang <guanran928@outlook.com>
2024-09-14 20:16:31 +0800

    mason: drop

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -23,7 +23,6 @@ return {
       return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
     end
 
-    require("mason").setup()
     require("luasnip.loaders.from_vscode").lazy_load()
 
     cmp.setup({
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,17 +1,8 @@
 return {
   "neovim/nvim-lspconfig",
-  dependencies = {
-    -- Portable package manager
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-  },
-
   init = function()
     local capabilities = require("cmp_nvim_lsp").default_capabilities() -- needed for nvim-cmp
-    require("mason").setup()
-    require("mason-lspconfig").setup()
 
-    -- lspconfig
     require("lspconfig").lua_ls.setup({
       capabilities = capabilities,
     })
