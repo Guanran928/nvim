21e917990e5dc2159c801ada3056aabc8a61e874
Guanran Wang <guanran928@outlook.com>
2024-09-14 19:22:44 +0800

    flake: remove package

--- flake.nix
+++ flake.nix
@@ -19,36 +19,17 @@
 
   outputs = inputs:
     inputs.flake-utils.lib.eachDefaultSystem (system: let
-      inherit (inputs.nixpkgs) lib;
       pkgs = inputs.nixpkgs.legacyPackages.${system};
       treefmtEval = inputs.treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
-
-      runtimeDeps = with pkgs; [
-        # mason / tree-sitter
-        gcc
-        cargo
-        nodejs
-
-        # telescope
-        ripgrep
-        fd
-      ];
-      src = lib.fileset.toSource {
-        fileset = lib.fileset.fileFilter (file: file.hasExt "lua") ./.;
-        root = ./.;
-      };
     in {
-      ### nix {run,shell,build}
-      packages.default = pkgs.callPackage ./package.nix {inherit runtimeDeps src;};
-
       ### nix fmt
       formatter = treefmtEval.config.build.wrapper;
 
       ### nix flake check
-      checks = {formatting = treefmtEval.config.build.check inputs.self;};
+      checks.formatting = treefmtEval.config.build.check inputs.self;
 
       ### nix develop
-      devShells.default = pkgs.mkShell {
+      devShells.default = pkgs.mkShellNoCC {
         nativeBuildInputs = with pkgs; [
           stylua
         ];
