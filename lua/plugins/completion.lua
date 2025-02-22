d0cb98210bf7850ee02a857fadd534690d43d0bd
Guanran Wang <guanran928@outlook.com>
2025-02-22 10:44:11 +0800

    blink.cmp: use luasnip

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,11 +1,24 @@
 ---@type LazyPluginSpec
 return {
   "saghen/blink.cmp",
-  dependencies = "rafamadriz/friendly-snippets",
   version = "*",
+
+  dependencies = {
+    "L3MON4D3/LuaSnip",
+    version = "v2.*",
+    dependencies = { "rafamadriz/friendly-snippets" },
+
+    init = function()
+      local luasnip = require("luasnip")
+      luasnip.filetype_extend("javascriptreact", { "html" })
+      luasnip.filetype_extend("typescriptreact", { "html" })
+      require("luasnip.loaders.from_vscode").lazy_load()
+    end,
+  },
 
   opts = {
     keymap = { preset = "enter" },
+    snippets = { preset = "luasnip" },
     completion = { list = { selection = { preselect = false } } },
     sources = {
       default = { "lazydev", "lsp", "path", "snippets", "buffer" },
