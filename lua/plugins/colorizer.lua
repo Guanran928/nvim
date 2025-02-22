f6046322426b8375ac87efdd04c2d1b6366bcff2
Guanran Wang <guanran928@outlook.com>
2025-02-22 11:11:01 +0800

    nvim-colorizer: refine config

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -2,10 +2,13 @@
 return {
   -- maintained fork of "norcalli/nvim-colorizer.lua"
   "catgoose/nvim-colorizer.lua",
-  event = "BufReadPre",
+  event = "VeryLazy",
   opts = {
+    lazy_load = true,
+    buftypes = { "*", "!nofile" },
     user_default_options = {
       names = false,
+      css = true,
       tailwind = true,
       mode = "virtualtext",
       virtualtext_inline = "before",
