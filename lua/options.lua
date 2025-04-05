34a3f1837a394f2363eaaf0dffc02321558468da
Guanran Wang <guanran928@outlook.com>
2025-04-06 03:41:38 +0800

    disable virtual_lines

--- lua/options.lua
+++ lua/options.lua
@@ -61,5 +61,16 @@ vim.api.nvim_create_autocmd("VimLeave", {
 })
 
 vim.diagnostic.config({
-  virtual_lines = true,
+  virtual_text = {
+    spacing = 0,
+    prefix = "",
+  },
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "",
+      [vim.diagnostic.severity.WARN] = "",
+      [vim.diagnostic.severity.INFO] = "",
+      [vim.diagnostic.severity.HINT] = "󰌵",
+    },
+  },
 })
