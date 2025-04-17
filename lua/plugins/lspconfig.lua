2e2c8e252cbfa74c695a0a2d4de8d9f4e853e402
Guanran Wang <guanran928@outlook.com>
2025-04-17 22:20:21 +0800

    lspconfig: simplify

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -7,32 +7,20 @@ return {
     local capabilities = require("blink.cmp").get_lsp_capabilities()
     local lspconfig = require("lspconfig")
 
-    lspconfig.lua_ls.setup({
-      capabilities = capabilities,
-    })
+    local servers = {
+      "gopls",
+      "lua_ls",
+      "nil_ls",
+      "pyright",
+      "rust_analyzer",
+      "tailwindcss",
+      "ts_ls",
+    }
 
-    lspconfig.pyright.setup({
-      capabilities = capabilities,
-    })
-
-    lspconfig.nil_ls.setup({
-      capabilities = capabilities,
-    })
-
-    lspconfig.gopls.setup({
-      capabilities = capabilities,
-    })
-
-    lspconfig.ts_ls.setup({
-      capabilities = capabilities,
-    })
-
-    lspconfig.tailwindcss.setup({
-      capabilities = capabilities,
-    })
-
-    lspconfig.rust_analyzer.setup({
-      capabilities = capabilities,
-    })
+    for _, lsp in pairs(servers) do
+      lspconfig[lsp].setup({
+        capabilities = capabilities,
+      })
+    end
   end,
 }
